0c8b069c2ab77d1523bdbd0c5d8a91350eccf0cf
Lucas Ambrosio <lu.amb99@gmail.com>
2022-12-02 17:03:38 -0300

    Desenvolvendo design responsivo

--- script.js
+++ script.js
@@ -16,3 +16,20 @@ function scrollsuave() {
   }
 }
 scrollsuave();
+
+/* Animação de rolagem rodapé pro inicio*/
+
+function scrolltoinit() {
+  const initfooter = document.querySelector(".toinit");
+  initfooter.addEventListener("click", scrolltoinit);
+  function scrolltoinit(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
+  }
+}
+scrolltoinit();
